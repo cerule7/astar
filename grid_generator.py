e3df6a2309ab74bbcaf335634344c761ab88f169
Charles Rule <cruleis@gmail.com>
2019-09-30 17:49:25 -0400

    Split grid and main up + pickling
    storing/loading grid list now possible
    (I've only generated 4 101x101 grids so far)

---
+++ grid_generator.py
@@ -0,0 +1,99 @@
+import numpy
+import random
+import pickle
+import os.path
+
+LENGTH = 101
+grid_list = []
+
+def generate_neighbors(cell):
+  neighbors = []
+  if(cell[0] - 1 >= 0):
+    neighbors.append([cell[0] - 1, cell[1]])
+  if(cell[0] + 1 < LENGTH):
+    neighbors.append([cell[0] + 1, cell[1]])
+  if(cell[1] - 1 >= 0):
+    neighbors.append([cell[0], cell[1] - 1])
+  if(cell[1] + 1 < LENGTH):
+    neighbors.append([cell[0], cell[1] + 1])
+  return neighbors
+
+def generate_grid():
+  global grid_list
+  grid = numpy.zeros((LENGTH, LENGTH))
+  visited = []
+  stack = []
+  current_cell = [random.randint(0, LENGTH - 1), random.randint(0, LENGTH - 1)]
+
+  while(len(visited) != (LENGTH ** 2)):
+    #mark current cell as visited
+    if(current_cell not in visited):
+      visited.append(current_cell)
+    
+    #generate neighbors of current cell
+    neighbors = generate_neighbors(current_cell)
+    neighbors = [n for n in neighbors if n not in visited]
+
+    # if multiple neighbors choose randomly, else choose 1st one, else backtrack, else go to a random cell
+    # if a cell is 1 it's blocked, if it's 0 it's unblocked
+    if(len(neighbors) > 1):
+      next_cell = neighbors[random.randint(0, len(neighbors) - 1)]
+      if(random.randint(0, 9) > 2):
+        stack.append(current_cell)
+      else:
+        grid[current_cell[0], current_cell[1]] = 1
+    elif(len(neighbors) == 1):
+      next_cell = neighbors[0]
+      if(random.randint(0, 9) > 2):
+        stack.append(current_cell)
+      else:
+        grid[current_cell[0], current_cell[1]] = 1
+    elif(len(stack) != 0):
+      next_cell = stack.pop()
+    else:
+      next_cell = [random.randint(0, LENGTH - 1), random.randint(0, LENGTH - 1)]
+      if(next_cell in visited and len(visited) != (LENGTH ** 2)):
+        while(next_cell in visited):
+          next_cell = [random.randint(0, LENGTH - 1), random.randint(0, LENGTH - 1)]
+      if(random.randint(0, 9) > 2):
+        stack.append(current_cell)
+      else:
+        grid[current_cell[0], current_cell[1]] = 1
+
+    current_cell = [next_cell[0], next_cell[1]]
+    if(len(visited) % 1000 == 0):
+      print('STEP # ' + str(len(visited)))
+
+  return grid
+
+def grid_in_list(grid, grid_list):
+    return next((True for elem in grid_list if numpy.array_equal(elem, grid)), False)
+
+def generate_grid_list():
+  global grid_list
+  while(len(grid_list) < 5):
+    print('GRID # ' + str(len(grid_list)))
+    grid = generate_grid()
+    if(not grid_in_list(grid, grid_list)):
+      grid_list.append(grid)
+
+def save_grid_list():
+  global grid_list
+  output_file = open('grids', 'wb')
+  pickle.dump(grid_list, output_file)
+  output_file.close()
+
+def load_grid_list():
+  global grid_list
+  if(os.path.isfile('grids')):
+    gridfile = open('grids', 'rb')
+    grid_list = pickle.load(gridfile)
+    gridfile.close()
+
+def get_grid_list():
+  global grid_list
+  return grid_list
+
+def get_random_grid():
+  global grid_list
+  return grid_list[random.randint(0, len(grid_list))]
--- main.py
+++ main.py
@@ -1,75 +1,12 @@
-import numpy
-import random 
+import state
+import os.path
+import grid_generator
 
-def generate_neighbors(cell):
-  neighbors = []
-  if(cell[0] - 1 >= 0):
-    neighbors.append([cell[0] - 1, cell[1]])
-  if(cell[0] + 1 < 101):
-    neighbors.append([cell[0] + 1, cell[1]])
-  if(cell[1] - 1 >= 0):
-    neighbors.append([cell[0], cell[1] - 1])
-  if(cell[1] + 1 < 101):
-    neighbors.append([cell[0], cell[1] + 1])
-  return neighbors
-
-def generate_grid():
-  grid = numpy.zeros((101, 101))
-  visited = []
-  stack = []
-  current_cell = [random.randint(0, 100), random.randint(0, 100)]
-
-  while(len(visited) != 10201):
-    #mark current cell as visited
-    if(current_cell not in visited):
-      visited.append(current_cell)
-    
-    neighbors = generate_neighbors(current_cell)
-    neighbors = [n for n in neighbors if n not in visited]
-
-    if(len(neighbors) > 1):
-      next_cell = neighbors[random.randint(0, len(neighbors) - 1)]
-      if(random.randint(0, 9) > 2):
-        stack.append(current_cell)
-      else:
-        grid[current_cell[0], current_cell[1]] = 1
-    elif(len(neighbors) == 1):
-      next_cell = neighbors[0]
-      if(random.randint(0, 9) > 2):
-        stack.append(current_cell)
-      else:
-        grid[current_cell[0], current_cell[1]] = 1
-    elif(len(stack) != 0):
-      next_cell = stack.pop()
-    else:
-      next_cell = [random.randint(0, 100), random.randint(0, 100)]
-      if(next_cell in visited and len(visited) != 10201):
-        while(next_cell in visited):
-          next_cell = [random.randint(0, 100), random.randint(0, 100)]
-      if(random.randint(0, 9) > 2):
-        stack.append(current_cell)
-      else:
-        grid[current_cell[0], current_cell[1]] = 1
-
-    current_cell = [next_cell[0], next_cell[1]]
-    if(len(visited) % 1000 == 0):
-      print('STEP # ' + str(len(visited)))
-
-  return grid
-
-def grid_in_list(grid, grid_list):
-    return next((True for elem in grid_list if numpy.array_equal(elem, grid)), False)
-
-grid_list = []
-
-while(len(grid_list) < 50):
-  print('GRID # ' + str(len(grid_list)))
-  grid = generate_grid()
-  if(not grid_in_list(grid, grid_list)):
-    grid_list.append(grid)
-
-for g in grid_list:
-  print(g)
-
-
-
+if(os.path.isfile('grids')):
+	grid_generator.load_grid_list()
+else:
+	grid_generator.generate_grid_list()
+	grid_generator.save_grid_list()
+	
+grid = grid_generator.get_random_grid()
+print(grid)
