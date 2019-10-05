d6e75de0b1251dd210bd2497d11541fdc53ab03a
Igloogloo <55815140+Igloogloo@users.noreply.github.com>
2019-10-05 17:39:05 -0400

    Add files via upload

---
+++ generalAStar.py
@@ -0,0 +1,8 @@
+import openList
+import hValue_gen
+import grid_generator
+import state
+import forwardastar
+import backwardastar
+#just decided to import everything for the time being
+def astar(start, goal, grid): #and other parameters if needed
